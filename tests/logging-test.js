cf8d4827576da3f91e348054c6129ce7eae66dd3
muffins <mluper@protonmail.com>
2020-12-08 13:34:07 -0500

    Added failing tests (TDD)

--- tests/logging-test.js
+++ tests/logging-test.js
@@ -7,15 +7,40 @@ export let LoggingTests = () => {
 
     describe('Logging Tests', function() { 
         describe('initialization', function() { 
-            it('should initialize logging when added as module');
+            it('should add a .log array to the dice roller on initialization', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                assert(Array.isArray(roller.log));
+            });
         });
 
         describe('logging', function() { 
-            it('should record results in a .log array');
-            it('should record the input in each log');
-            it('should record the output in each log');
-            it('should record rolls in a .log[n].rolls array')
-            it('should record the roll expression and output array')
+            it('should record results in a .log array', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                roller.solve('3d6');
+                roller.solve('1d4');
+                assert(roller.log.length === 2);
+            });
+            it('should record the input in each log', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                roller.solve('1d4');
+                assert(roller.log.slice(-1).input === '1d4');
+            });
+            it('should record the output in each log', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                roller.solve('4d1');
+                assert(roller.log.slice(-1).output === '4');
+            });
+            it('should record rolls in a .log[n].rolls array', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                roller.solve('3d8+2d4');
+                assert(roller.log.slice(-1).rolls.length === 2);
+            });
+            it('should record the roll expression and output array', function() { 
+                let roller = new DiceRoller({ modules : [LoggingModule]});
+                roller.solve('2d10');
+                assert(roller.log.slice(-1).rolls[0].expression === '2d10');
+                assert(roller.log.slice(-1).rolls[0].results.length === 2);
+            });
         });
 
     });
