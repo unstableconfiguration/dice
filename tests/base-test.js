054247444afb3b6fd61ff61cfeab8868875b3c17
muffins <mluper@protonmail.com>
2020-12-08 15:50:30 -0500

    filling out tests, 1 failing due to singleton

--- tests/base-test.js
+++ tests/base-test.js
@@ -5,10 +5,34 @@ export let BaseTests = function() {
     
     describe('Base Functionality', function() { 
         describe('Hooks', function() { 
-            it('should call .onSolved when .solve() is called');
-            it('should call .onSearched when an operation searches the input');
-            it('should call .onParsed when an operation parses the input');
+            it('should call .onSolved when .solve() is called', function(done) { 
+                let roller = new DiceRoller();
+                roller.onSolved = function(input, output) {
+                    assert(input === '3d4');
+                    done();
+                }
+                roller.solve('3d4');
+            });
+            it('should call .onSearched when an operation searches the input', function(done) { 
+                let roller = new DiceRoller();
+                roller.operations[0].onSearched = function(input, found) {
+                    assert(input == '2d8' && found == '2d8');
+                    done();
+                }
+                roller.solve('2d8');
+            });
+            it('should call .onParsed when an operation parses the input', function(done) { 
+                let roller = new DiceRoller();
+                roller.operations[0].onParsed = function(searchResult, operands) { 
+                    console.log(searchResult, operands);
+                    assert(searchResult == '5d12' && operands[0] == 5);
+                    done();
+                }
+                roller.solve('5d12');
+            });
             it('should call .onEvaluated when an operation evaluates the input');
+
+            
             
         });
     });
