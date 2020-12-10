9dc6822804e87f3273f9b992a10c7bdf27ca5d04
muffins <mluper@protonmail.com>
2020-12-10 09:23:42 -0500

    renaming build->deploy and adding logging module

--- build/build.bat
+++
@@ -1,4 +0,0 @@
-:: Combine all the es6 modules into a single file 
-powershell -Command "& {cat ../dice.js, ../dice-operation.js, ../modules/base-module.js, ../modules/dnd-module.js, ../modules/math-module.js | sc dice.js}"
-:: Remove unnecessary import statements
-powershell -Command "(gc dice.js) -replace 'import.*', '' | Out-File -encoding ASCII dice.js"
