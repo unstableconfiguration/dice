cc2a71b86c67a96563e468b10f1fa6ede993927d
oldmanmuffins <mluper.official@protonmail.com>
2021-09-05 20:11:24 -0400

    fixing build path

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import del from 'rollup-plugin-delete';
 import babel from '@rollup/plugin-babel';
 
 export default [{
-        input: 'src/index.js',
+        input: 'index.js',
         output : [
             { 
                 file : 'dist/dice.js',
