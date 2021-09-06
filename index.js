f144ed5c17f0b94a9f01a065a452c99938ad6e6a
oldmanmuffins <mluper.official@protonmail.com>
2021-09-05 20:09:51 -0400

    rehousing index.js and renaming src->app

---
+++ index.js
@@ -0,0 +1,13 @@
+import { DiceRoller } from './app/dice.js';
+
+import { DnDModule } from './app/modules/dnd-module.js';
+import { MathModule } from './app/modules/math-module.js';
+import { LoggingModule } from './app/modules/logging-module.js'
+
+
+export { 
+    DiceRoller,
+    DnDModule,
+    MathModule,
+    LoggingModule
+}
